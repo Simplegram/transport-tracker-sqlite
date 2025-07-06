50f7f7271ff1154445e43467b7c22732c8fff310
Simplegram <nkgnicholas@gmail.com>
2025-07-06 11:25:36 +0700

    fix: first-time db migration error

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,9 @@ export default function Home() {
 
     useEffect(() => {
         migrateDb()
+    }, [])
+
+    useEffect(() => {
         if (isMigrating === false) router.push('/main')
     }, [isMigrating])
 
