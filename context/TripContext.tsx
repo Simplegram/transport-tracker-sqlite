0961175057785885631160d1b14d3bd9d078fb17
Simplegram <nkgnicholas@gmail.com>
2025-07-23 08:56:10 +0700

    fix: type error

--- context/TripContext.tsx
+++ context/TripContext.tsx
@@ -1,18 +1,19 @@
 import { CompleteTrip } from "@/src/types/CompleteTypes"
+import { Trip } from "@/src/types/Types"
 import { createContext, PropsWithChildren, useContext, useState } from "react"
 
 interface TripContextValue {
     tripId: number | null
     setTripId: (id: number) => void
-    currentTrip: CompleteTrip | null
-    setCurrentTrip: (trip: CompleteTrip | null) => void
+    currentTrip: Trip | CompleteTrip | null
+    setCurrentTrip: (trip: Trip | CompleteTrip | null) => void
 }
 
 export const TripContext = createContext<TripContextValue | null>(null)
 
 export const TripProvider = ({ children }: PropsWithChildren) => {
     const [tripId, setTripId] = useState<number | null>(null)
-    const [currentTrip, setCurrentTrip] = useState<CompleteTrip | null>(null)
+    const [currentTrip, setCurrentTrip] = useState<Trip | CompleteTrip | null>(null)
 
     return (
         <TripContext.Provider value={{
